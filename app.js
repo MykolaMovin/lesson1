b95d1f41c053a005dd0d72cb8736e3ff52d13265
MykolaMovin <149692103+MykolaMovin@users.noreply.github.com>
2023-11-03 11:00:34 +0200

    Updating lesson1

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ console.log('Movin');
 let firstVariable, secondVariable;
 firstVariable = 42;
 secondVariable = 'Hello World!';
-alert(firstVariable, secondVariable);
+alert(`${firstVariable}, ${secondVariable}`);
 secondVariable = firstVariable;
-alert(firstVariable, secondVariable);
+alert(`${firstVariable}, ${secondVariable}`);
 // Practical task 4
 let firstObject = {
     String: 'String',
@@ -17,23 +17,27 @@ let firstObject = {
     Null: null
 };
 // Practical task 5
-let isAdult = confirm('Do you reach 18?');
+let isAdult = confirm('I am 18 or older');
+console.log(isAdult);
 // Practical task 6
 let firstName = typeof 'Mykola';
 let lastName = typeof 'Movin'; 
 let studyGroup = typeof 'JavaScript for TA [Q4, 2023]';
 const yearOfBirth = typeof 1999;
-let isMarriage = typeof false;
-console.log(yearOfBirth, isMarriage, firstName);
+let isMarried = typeof false;
+console.log(yearOfBirth, isMarried, firstName);
+let undefinedVariable;
+let nullVariable = null;
+console.log(typeof undefinedVariable, typeof nullVariable);
 // Practical task 7
-let userName = prompt('Please, enter your Name');
-let email = prompt('Pleas, enter your Email');
-let password = prompt('Please, enter your password');
-let message = alert(`"Dear ${userName}, your email is ${email}, your password is ${password}`)
+let userName = prompt('Please, enter your Name', 'Mykola');
+let email = prompt('Pleas, enter your Email', 'example@gmail.com');
+let password = prompt('Please, enter your password', 'strongPassword');
+let message = alert(`"Dear ${userName}, your email is ${email}, your password is ${password}`);
 // Practical task 8
 let second = 1;
 let minute = 60 * second;
 let hour = 60 * minute;
 let day = 24 * hour;
 let month = 30 * day;
-console.log(hour, day, month)
+console.log(hour, day, month);
